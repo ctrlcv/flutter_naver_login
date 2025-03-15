b99e892ff34b6e49e21af4031ba7fee3cb6ad6db
Hyungoo Kim <ctrlcvis@gmail.com>
2025-03-15 20:53:58 +0900

    data objects 이슈 수정

--- android/src/main/kotlin/com/yoonjaepark/flutter_naver_login/FlutterPluginMethod.kt
+++ android/src/main/kotlin/com/yoonjaepark/flutter_naver_login/FlutterPluginMethod.kt
@@ -1,25 +1,59 @@
 package com.yoonjaepark.flutter_naver_login
 
 sealed class FlutterPluginMethod(val methodName: String) {
-    data object InitSdk : FlutterPluginMethod("initSdk")
-    data object LogIn : FlutterPluginMethod("logIn")
-    data object LogOut : FlutterPluginMethod("logOut")
-    data object LogOutAndDeleteToken : FlutterPluginMethod("logoutAndDeleteToken")
-    data object GetCurrentAccount : FlutterPluginMethod("getCurrentAccount")
-    data object GetCurrentAccessToken : FlutterPluginMethod("getCurrentAccessToken")
-    data object RefreshAccessTokenWithRefreshToken :
-        FlutterPluginMethod("refreshAccessTokenWithRefreshToken")
+    class InitSdk private constructor() : FlutterPluginMethod("initSdk") {
+        companion object {
+            val INSTANCE = InitSdk()
+        }
+    }
+    
+    class LogIn private constructor() : FlutterPluginMethod("logIn") {
+        companion object {
+            val INSTANCE = LogIn()
+        }
+    }
+    
+    class LogOut private constructor() : FlutterPluginMethod("logOut") {
+        companion object {
+            val INSTANCE = LogOut()
+        }
+    }
+    
+    class LogOutAndDeleteToken private constructor() : FlutterPluginMethod("logoutAndDeleteToken") {
+        companion object {
+            val INSTANCE = LogOutAndDeleteToken()
+        }
+    }
+    
+    class GetCurrentAccount private constructor() : FlutterPluginMethod("getCurrentAccount") {
+        companion object {
+            val INSTANCE = GetCurrentAccount()
+        }
+    }
+    
+    class GetCurrentAccessToken private constructor() : FlutterPluginMethod("getCurrentAccessToken") {
+        companion object {
+            val INSTANCE = GetCurrentAccessToken()
+        }
+    }
+    
+    class RefreshAccessTokenWithRefreshToken private constructor() : 
+        FlutterPluginMethod("refreshAccessTokenWithRefreshToken") {
+        companion object {
+            val INSTANCE = RefreshAccessTokenWithRefreshToken()
+        }
+    }
 
     companion object {
         fun fromMethodName(name: String?): FlutterPluginMethod? {
             return when (name) {
-                InitSdk.methodName -> InitSdk
-                LogIn.methodName -> LogIn
-                LogOut.methodName -> LogOut
-                LogOutAndDeleteToken.methodName -> LogOutAndDeleteToken
-                GetCurrentAccount.methodName -> GetCurrentAccount
-                GetCurrentAccessToken.methodName -> GetCurrentAccessToken
-                RefreshAccessTokenWithRefreshToken.methodName -> RefreshAccessTokenWithRefreshToken
+                InitSdk.INSTANCE.methodName -> InitSdk.INSTANCE
+                LogIn.INSTANCE.methodName -> LogIn.INSTANCE
+                LogOut.INSTANCE.methodName -> LogOut.INSTANCE
+                LogOutAndDeleteToken.INSTANCE.methodName -> LogOutAndDeleteToken.INSTANCE
+                GetCurrentAccount.INSTANCE.methodName -> GetCurrentAccount.INSTANCE
+                GetCurrentAccessToken.INSTANCE.methodName -> GetCurrentAccessToken.INSTANCE
+                RefreshAccessTokenWithRefreshToken.INSTANCE.methodName -> RefreshAccessTokenWithRefreshToken.INSTANCE
                 else -> null
             }
         }
